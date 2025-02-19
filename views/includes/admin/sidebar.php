88f95543345305180258e2b30354dfce23d03976
Lydavid Ho <lydavidho@Lydavids-MacBook-Pro.local>
2025-02-19 14:45:57 +0700

    structure code

---
+++ views/includes/admin/sidebar.php
@@ -0,0 +1,112 @@
+<div class="sidebar" data-background-color="dark">
+  <div class="sidebar-logo">
+    <!-- Logo Header -->
+    <div class="logo-header" data-background-color="dark">
+      <a href="dashboard.php" class="logo">
+        <img
+          src="../assets/Image/logo.png"
+          alt="navbar brand"
+          class="navbar-brand h-100"
+          height="20" />
+      </a>
+      <div class="nav-toggle">
+        <button class="btn btn-toggle toggle-sidebar">
+          <i class="gg-menu-right"></i>
+        </button>
+        <button class="btn btn-toggle sidenav-toggler">
+          <i class="gg-menu-left"></i>
+        </button>
+      </div>
+      <button class="topbar-toggler more">
+        <i class="gg-more-vertical-alt"></i>
+      </button>
+    </div>
+    <!-- End Logo Header -->
+  </div>
+  <div class="sidebar-wrapper scrollbar scrollbar-inner">
+    <div class="sidebar-content">
+      <ul class="nav nav-secondary">
+        <li class="nav-item active">
+          <a
+            data-bs-toggle="collapse"
+            href="#dashboard"
+            class="collapsed"
+            aria-expanded="false">
+            <i class="fas fa-home"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-section">
+          <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+          </span>
+          <h4 class="text-section">Components</h4>
+        </li>
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#base">
+            <i class="fas fa-th-list"></i>
+            <p>SlideShow</p>
+            
+          </a>
+
+        </li>
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#sidebarLayouts">
+          <i class="fa fa-box"></i>
+            <p>Product</p>
+            
+          </a>
+        </li>
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#forms">
+          <i class="fa fa-briefcase"></i>
+            <p>Categories</p>
+            
+          </a>
+        </li>
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#tables">
+          <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+            <p>Brands</p>
+            
+          </a>
+        </li>
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#maps">
+          <i class="fa fa-file" aria-hidden="true"></i>
+            <p>Pages</p>
+            
+          </a>
+        </li>
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#charts">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <p>User</p>
+            
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="widgets.html">
+          <i class="fas fa-credit-card"></i>
+            <p>Payments</p>
+            <span class="badge badge-success">4</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="dashboard.php">
+            <i class="fas fa-file"></i>
+            <p>Documentation</p>
+            <span class="badge badge-secondary">1</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#submenu">
+            <i class="fas fa-bars"></i>
+            <p>Menu Levels</p>
+            
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
